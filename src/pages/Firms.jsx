a0dac39705bef6fc1b163320fd25dba66a7c1bab
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-08-09 15:47:12 +0300

    39. Updated Firm.jsx

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,10 +1,13 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useStockCall from "../hooks/useStockCall";
+import FirmCard from "../components/FirmCard";
 const Firms = () => {
   const { getStockData } = useStockCall();
+
+  const { firms } = useSelector((state) => state.stock);
 
   useEffect(() => {
     getStockData("firms");
@@ -16,6 +19,14 @@ const Firms = () => {
         Firm
       </Typography>
       <Button variant="contained">New Firm</Button>
+
+      <Grid container>
+        {firms?.map((firm) => (
+          <Grid item key={firm.id}>
+            <FirmCard firm={firm} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
