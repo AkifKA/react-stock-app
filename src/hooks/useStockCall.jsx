11d4d64e4c7c37f5a1e8edfd4e147ac16fc72261
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-08-09 14:55:04 +0300

    35. Updated useStockCall.jsx

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -9,7 +9,6 @@ const useStockCall = () => {
   const getStockData = async (url) => {
     const BASE_URL = "https://10001.fullstack.clarusway.com/";
     dispatch(fetchStart());
-
     try {
       const { data } = await axios(`${BASE_URL}stock/${url}/`, {
         headers: { Authorization: `Token ${token}` },
@@ -20,7 +19,21 @@ const useStockCall = () => {
       dispatch(fetchFail());
     }
   };
-  return { getStockData };
+  const deleteStockData = async (url, id) => {
+    const BASE_URL = "https://10001.fullstack.clarusway.com/";
+    dispatch(fetchStart());
+    try {
+      await axios.delete(`${BASE_URL}stock/${url}/${id}/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      getStockData(url);
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
+  return { getStockData, deleteStockData };
 };
 
 export default useStockCall;
