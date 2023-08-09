764fbf4b6fbca0050a89c7bf86b54af0f3756a89
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-08-09 14:20:51 +0300

    31. Built useStockCall.jsx

---
+++ src/hooks/useStockCall.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStart, getSuccess } from "../features/stockSlice";
+import { fetchFail } from "../features/stockSlice";
+const useStockCall = () => {
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const getStockData = async (url) => {
+    const BASE_URL = "https://10001.fullstack.clarusway.com/";
+    dispatch(fetchStart());
+
+    try {
+      const { data } = await axios(`${BASE_URL}stock/${url}/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      dispatch(getSuccess({ data, url }));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+  return { getStockData };
+};
+
+export default useStockCall;
