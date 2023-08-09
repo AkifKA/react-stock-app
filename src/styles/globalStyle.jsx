c796e03cf000df3db840ed9ab3093e3b8bbdf389
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-08-09 15:50:49 +0300

    40. Built globalStyle.jsx

---
+++ src/styles/globalStyle.jsx
@@ -0,0 +1,11 @@
+export const flex = {
+  display: "flex",
+  justifyContent: "center",
+  alignItem: "center",
+  gap: 1,
+};
+
+export const btnStyle = {
+  cursor: "pointer",
+  "&:hover": { color: "red" },
+};
